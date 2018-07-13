bc9aa689b2520c62ce5ca68cde613001cdd83bf8
Aaron Hourie <aaronhourie@gmail.com>
2018-07-13 15:21:54 -0300

    Fixed gitignore

--- Debug/objects.mk
+++
@@ -1,7 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-LIBS := $(GEN_CMDS_QUOTED) -l"libc.a" -l"ezdsp5535bsl.lib" -l"rts55x.lib" -l"cslVC5505.lib"
-
-USER_OBJS :=
